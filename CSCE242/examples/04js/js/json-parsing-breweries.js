c57ec8cc967c4378e996df2d245d413aa97d78db
KiNgChAzY <158865021+KiNgChAzY@users.noreply.github.com>
2025-06-02 11:39:23 -0400

    json

--- CSCE242/examples/04js/js/json-parsing-breweries.js
+++ CSCE242/examples/04js/js/json-parsing-breweries.js
@@ -1,1 +1,52 @@
-//https://api.openbrewerydb.org/breweries
+//https://www.openbrewerydb.org/documentation
+
+const getBreweries = async() => {
+    const url = "https://api.openbrewerydb.org/v1/breweries";
+
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+const showBreweries = async() => {
+    const breweries = await getBreweries();
+    const breweriesDiv = document.getElementById("breweries");
+
+    breweries.forEach((brewery) => {
+        const brewerySection = document.createElement("section");
+        breweriesDiv.append(breweriesSection);
+
+        //make the h3
+        const h3 = document.createElement("h3");
+        h3.innerHTML = brewery.name;
+        brewerySection.append(h3);
+
+        //make the brand paragraph
+        const p = document.createElement("p");
+        p.innerHTML = `id: ${brewery.id}`;
+        brewerySection.append(p);
+
+
+        const p2 = document.createElement("p");
+        p.innerHTML = `brewery type: ${brewery.brewery_type}`;
+        brewerySection.append(p);
+
+        //make the reviews list
+        const ul = document.createElement("ul");
+        brewerySection.append(ul);
+
+        shoe.reviews.forEach((review) => {
+            const li = document.createElement("li");
+            li.innerHTML = review;
+            ul.append(li);
+        });
+
+    });
+};
+
+showBreweries();
---
+++ CSCE242/projects/part4/js/json.js
@@ -0,0 +1,40 @@
+[{
+        "_id": 1,
+        "Address": "124 easy street",
+        "Listing-Price": "575,000$",
+        "Stories": "2",
+        "Square Footage": "2000sqft",
+        "Bedrooms&Bathrooms": "3-2",
+        "Property-Type": "Condo",
+        "Year-Built": "2010",
+        "Lot Size": "1Acer",
+        "Features": "Pool, fireplace, and garage",
+        "Photos": "URLs"
+    },
+    {
+        "_id": 2,
+        "Address": "",
+        "Listing-Price": "",
+        "Stories": "",
+        "Square Footage": "",
+        "Bedrooms&Bathrooms": "",
+        "Property-Type": "",
+        "Year-Built": "",
+        "Lot Size": "2000sqft",
+        "Features": "Pool, fireplace, and garage",
+        "Photos": "URLs"
+    },
+    {
+        "_id": 3,
+        "Address": "",
+        "Listing-Price": "",
+        "Stories": "",
+        "Square Footage": "",
+        "Bedrooms&Bathrooms": "",
+        "Property-Type": "",
+        "Year-Built": "",
+        "Lot Size": "",
+        "Features": "",
+        "Photos": ""
+    }
+]
